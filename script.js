c2077e2bf0411e17896f198303c067f0c37ed67d
tod619 <trevorodwyer619@hotmail.com>
2023-02-12 21:13:31 +0000

    Update date h1 with date object month in script.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+const date = new Date()
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
+document.querySelector('.date h1').innerHTML = months[date.getMonth()]

ca1bd2a752c4e1eca8de532cc16fad7f820106e4
tod619 <trevorodwyer619@hotmail.com>
2023-02-12 21:33:24 +0000

    Display days of month using date object in script.js file

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const date = new Date()
 
+const monthDays = document.querySelector('.days')
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+
+console.log(lastDay)
+
 const months = [
     'January',
     'February',
@@ -20,3 +26,12 @@ document.querySelector('.date h1').innerHTML = months[date.getMonth()]
 
 // Display Date in P
 document.querySelector('.date p').innerHTML = date.toDateString()
+
+// create calendar days
+let days = ''
+
+for(let i = 1; i <= lastDay; i++) {
+    days += `<div>${i}</div>`
+}
+
+monthDays.innerHTML = days 
